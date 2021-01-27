4c0af5f8457d13f43f240fc70a9fb9f5b23ef6b6
mallabs <78091225+mallabs@users.noreply.github.com>
2021-01-27 23:06:39 +0100

    Add files via upload

---
+++ public/d64684d8-25bbb34a5ee8bf623c7d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{PRV2:function(n,t,o){"use strict";o.d(t,"a",(function(){return e}));var r=o("Lnxd");function e(n){return Object(r.a)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M9 5l7 7-7 7"}}]})(n)}}}]);
+//# sourceMappingURL=d64684d8-25bbb34a5ee8bf623c7d.js.map
